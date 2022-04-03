9c390bc392894b186993f8b14fd718a77cdd2733
roger <tuyideveloper@gmail.com>
2022-04-03 21:41:46 +0200

    complete task 3 : detail a todo

--- Pages/Detail.cshtml.cs
+++ Pages/Detail.cshtml.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using todolistApp.Services;
+using todolistApp.Models;
 
 namespace todolistApp.Pages
 {
     public class DetailModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public int askedTodoId { get; set; }
+
+        public Todo? askedTodo { get; set; }
+
+        public void OnGet(int id)
         {
+            askedTodoId = id;
+            askedTodo = TodoService.GetATodo(askedTodoId);
         }
+
+
     }
 }
--- Services/TodoService.cs
+++ Services/TodoService.cs
@@ -9,11 +9,25 @@ static class TodoService
     static TodoService()
     {
         TodoList = new List<Todo> {
-            new Todo{Id=1, Title = "toto", Description = "ange", IsTaskComplete = false},
-            new Todo{Id=2, Title = "daoa", Description = "oddo", IsTaskComplete = true},
+            new Todo{Id=1, Title = "Toulouse", Description = "visite de la ville rose", IsTaskComplete = false},
+            new Todo{Id=2, Title = "code", Description = "apprendre le c#", IsTaskComplete = true},
         };
     }
 
     public static List<Todo>? GetTodolist() { return TodoList; }
+
+    public static Todo GetATodo(int id)
+    {
+        List<Todo>? list = GetTodolist();
+        foreach (Todo todo in list)
+        {
+            if (todo.Id == id)
+            {
+                return todo;
+            }
+        }
+        //return an Error 404?
+        return null;
+    }
 
 }
